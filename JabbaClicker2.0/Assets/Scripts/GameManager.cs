08fdf485cd31261afaeaad75c8df81cf72e5aadb
GeorgeGerontides <132400235+GeorgeGerontides@users.noreply.github.com>
2023-05-17 21:53:58 -0700

    additions

--- JabbaClicker2.0/Assets/Scripts/GameManager.cs
+++ JabbaClicker2.0/Assets/Scripts/GameManager.cs
@@ -6,13 +6,25 @@ public class GameManager : MonoBehaviour
 {
     public static int jabbaScore;
     public static int scoreMultiplier;
+    public static int score;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreMultiplier = 1;
         jabbaScore = 0;
+
+        // Start invoking the AddScore method every second
+        InvokeRepeating("AddScore", 1f, 1f);
     }
 
-    
+    private void AddScore()
+    {
+        jabbaScore++; // Increment the jabbaScore
+    }
+
+    public void AddPoint(int points)
+    {
+        score += points;
+    }
 }
